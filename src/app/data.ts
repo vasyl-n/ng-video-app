3dccc6b597d0a0cd8090885a8aa60d4e2fe7b9fa
vasyl-n <vasyl.nrk@gmail.com>
2018-11-05 23:28:52 -0800

    addds file with example data

---
+++ src/app/data.ts
@@ -0,0 +1,64 @@
+const data = [
+  {
+      "snippet": "Mm-hmm",
+      "speaker": "Cust",
+      "time": 1.92
+  },
+  {
+      "snippet": "Hello?",
+      "speaker": "Rep",
+      "time": 3.66
+  },
+  {
+      "snippet": "Yeah,",
+      "speaker": "Cust",
+      "time": 62.249399999999994
+  },
+  {
+      "snippet": "Hello?",
+      "speaker": "Cust",
+      "time": 63.979299999999995
+  },
+  {
+      "snippet": "Cool",
+      "speaker": "Rep",
+      "time": 41.789699999999996
+  },
+  {
+      "snippet": "Yeah,",
+      "speaker": "Cust",
+      "time": 94.621
+  },
+  {
+      "snippet": "Mm-hmm",
+      "speaker": "Rep",
+      "time": 69.28960000000001
+  },
+  {
+      "snippet": "Don't simple",
+      "speaker": "Rep",
+      "time": 66.4694
+  },
+  {
+      "snippet": "In the new gen",
+      "speaker": "Rep",
+      "time": 16.43
+  },
+  {
+      "snippet": "And by the window experiment",
+      "speaker": "Rep",
+      "time": 5.78
+  },
+  {
+      "snippet": "Just list most that's him",
+      "speaker": "Cust",
+      "time": 21.38
+  },
+  {
+      "snippet": "Sorry I'm out on your chance",
+      "speaker": "Rep",
+      "time": 10.13001
+  }
+]
+
+export default data
