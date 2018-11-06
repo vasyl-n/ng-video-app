8b62736aecd50b8f5a7106e9a9c7970d22c65108
vasyl-n <vasyl.nrk@gmail.com>
2018-11-05 23:32:09 -0800

    updates app component with css and new modules added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { HttpClientModule }    from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VideoPlayerComponent } from './video-player/video-player.component';
+import { ContentContainerComponent } from './content-container/content-container.component';
+import { HeaderComponent } from './header/header.component';
+import { TranscriptComponent } from './transcript/transcript.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoPlayerComponent,
+    ContentContainerComponent,
+    HeaderComponent,
+    TranscriptComponent
   ],
   imports: [
     BrowserModule,
